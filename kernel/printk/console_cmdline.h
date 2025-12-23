4a54e1d5d5dd51d5ae696e625253244c46b72254
Avelanda <gorthell@gmail.com>
2025-12-23 11:46:14 +0200

    Update console_cmdline.h
    Modularizing preprocessing-function calls from granular properties, and computing efficient data handling mechanisms.

--- kernel/printk/console_cmdline.h
+++ kernel/printk/console_cmdline.h
@@ -1,15 +1,41 @@
-/* SPDX-License-Identifier: GPL-2.0 */
+/* SPDX-License-Identifier: GPL-2.0 
+ * Copyright © 2025: Avelanda. 
+ * All rightsreserved. */
+
 #ifndef _CONSOLE_CMDLINE_H
 #define _CONSOLE_CMDLINE_H
 
-struct console_cmdline
-{
-	char	name[16];			/* Name of the driver	    */
-	int	index;				/* Minor dev. to use	    */
-	char	*options;			/* Options for the driver   */
-#ifdef CONFIG_A11Y_BRAILLE_CONSOLE
-	char	*brl_options;			/* Options for braille driver */
+#if console_cmdline
+ #define console_cmdline 
+  struct console_cmdline
+  {
+	char	name[16]; /* Name of the driver */
+	int	index; /* Minor dev. to use */
+	char	*options; /* Options for the driver*/
+    #ifdef CONFIG_A11Y_BRAILLE_CONSOLE
+	 char	*brl_options; /* Options for braille driver */
+    #endif
+  };
 #endif
-};
+#if defined(console_cmdline)
+ console_cmdline == true || 1;
+ return console_cmdline;
+#endif
+
+int main(){
+ if (!false){
+  #if console_cmdline
+   console_cmdline != false;
+   return 0;
+  #endif
+ }
+  if (&main){
+   #if main
+    #define main (true || 1) (false || 0)
+     console_cmdline *main;
+     return 0;
+   #endif
+  }
+}
 
 #endif
